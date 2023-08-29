370b0ba2cfc8cfca3782d8e24f2590632db713ff
MuriloRoveri <RM97893@fiap.com.br>
2023-08-29 11:50:06 -0300

    a

--- React-fiap/src/components/Rodape.jsx
+++ React-fiap/src/components/Rodape.jsx
@@ -1,15 +1,16 @@
 export default function Rodape(props) {
-    return (
-      <>
-        <footer>
-          
-        <ul>
-            {props}
-        </ul>
-  
-          <p>&copy; 2021, Vite.js and React.</p>
-        </footer>
-      </>
-    );
-  }
-  
+  return (
+    <>
+      <footer>
+        
+      <ul>
+          {props.listaProps.map( (li)=>{
+              return li;
+      })}
+      </ul>
+
+        <p>&copy; 2021, Vite.js and React.</p>
+      </footer>
+    </>
+  );
+}
